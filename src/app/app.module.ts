f0311c1f670ae9f3369833e81a86a318d4b9267e
Nidhi <nithiarun@yahoo.com>
2019-05-20 20:13:10 -0700

    Created new component tv-show-details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TVShowDetailsComponent } from './tv-show-details/tv-show-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TVShowDetailsComponent
   ],
   imports: [
     BrowserModule,
