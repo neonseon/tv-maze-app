397e6fda3d67ff0554095b15b82c9569cade836d
Seon Ricks <skcirs@gmail.com>
2019-05-21 18:53:03 -0700

    refactor: Add test values to component

--- src/app/tv-show-details/tv-show-details.component.ts
+++ src/app/tv-show-details/tv-show-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ITVShowDetails } from '../itvshow-details';
 
 @Component({
   selector: 'app-tv-show-details',
@@ -7,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TVShowDetailsComponent implements OnInit {
 
-  constructor() { }
+  show: ITVShowDetails
+  constructor() {
+    this.show = {
+      title: 'The Mindy Project',
+      image: 'https://tvseriesfinale.com/wp-content/uploads/2016/05/The-Mindy-Project-TV-show-on-Hulu-season-5-renewal-canceled-or-renewed-590x332.jpeg',
+      description: 'A young Ob/Gyn doctor balances her personal and professional life, surrounded by quirky co-workers in a small office.',
+      cast: 'Mindy Kaling, Ike Barinholtz, Ed Weeks',
+      rating: 8,
+      genre: 'Comedy',
+      id: 912      
+    }
+   }
 
   ngOnInit() {
   }
