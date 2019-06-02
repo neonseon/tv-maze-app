a195e364552104a6be685534091596c82034c513
Seon Ricks <skcirs@gmail.com>
2019-06-01 20:15:23 -0700

    refactor: Create getTvShowCast function + set up cast in ITvShow interface

--- src/app/tv-show/tv-show.service.ts
+++ src/app/tv-show/tv-show.service.ts
@@ -6,16 +6,17 @@ import { ITVShowDetails } from '../itvshow-details';
 import {map} from 'rxjs/operators';
 
 interface ITvShow{
-    name:string,
-    id:number,
-    genres:string,
-    summary:string,
-    image:{
-      medium:string
-    }
-    rating:{
-      average:number
-    }
+  name:string,
+  id:number,
+  genres:string,
+  summary:string,  
+  image:{
+    medium:string
+  },
+  rating:{
+    average:number
+  },
+  cast:string 
 }
 
 @Injectable({
@@ -29,15 +30,18 @@ export class TvShowService {
   getTvShow(name:string){
     return this.httpClient.get<ITvShow>(`${environment.baseUrl}api.tvmaze.com/singlesearch/shows?q=${name}`).pipe(map(data=>this.transformToITvShowDetails(data)))
   }
+  getTvShowCast(id:number){
+    return this.httpClient.get<ITvShow>(`${environment.baseUrl}api.tvmaze.com/shows/${id}/cast`)
+  }  
   private transformToITvShowDetails(data:ITvShow):ITVShowDetails{
     return{
       title:data.name,
       image:data.image.medium,
       description:data.summary,
-      cast:'Mindy Kaling, Ike Barinholtz, Ed Weeks',
       rating:data.rating.average,
       genre:data.genres,
-      id:data.id
+      id:data.id,
+      cast:data.cast
     }
   }
 }
