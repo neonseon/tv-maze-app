0936d4051522e187b72a86bc3becfa26c32123f9
Seon Ricks <skcirs@gmail.com>
2019-06-29 08:29:00 -0700

    test: Add unit tests for AppComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,33 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import{ MatFormFieldModule, MatInputModule, MatButtonModule, MatToolbarModule, MatCardModule, MatIconModule} from '@angular/material';
 import { AppComponent } from './app.component';
+import { TvShowSearchComponent } from './tv-show-search/tv-show-search.component';
+import { TVShowDetailsComponent } from './tv-show-details/tv-show-details.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatCardModule,
+        MatIconModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientTestingModule,
+        BrowserAnimationsModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        TvShowSearchComponent,
+        TVShowDetailsComponent
       ],
     }).compileComponents();
   }));
@@ -26,10 +44,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('tv-maze-app');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render title in a h2 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to tv-maze-app!');
+    expect(compiled.querySelector('h2').textContent).toContain('Find a TV Show');
   });
 });
