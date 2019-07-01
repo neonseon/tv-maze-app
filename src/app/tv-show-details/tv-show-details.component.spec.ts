133b94f0022614e14fb5b874763c23cd485432db
Nidhi <nithiarun@yahoo.com>
2019-06-30 22:37:44 -0700

    Unit test for tv-show-details.component.spec.ts

--- src/app/tv-show-details/tv-show-details.component.spec.ts
+++ src/app/tv-show-details/tv-show-details.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { TVShowDetailsComponent } from './tv-show-details.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import{ MatFormFieldModule, MatInputModule, MatButtonModule, MatToolbarModule, MatCardModule, MatIconModule} from '@angular/material';
 
 describe('TVShowDetailsComponent', () => {
   let component: TVShowDetailsComponent;
@@ -8,6 +12,19 @@ describe('TVShowDetailsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatCardModule,
+        MatIconModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientTestingModule,
+        BrowserAnimationsModule
+      ],
       declarations: [ TVShowDetailsComponent ]
     })
     .compileComponents();
