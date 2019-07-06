b345d16fed2aa5f75af9cc9cd40c938f737cc641
MoGood <50001536+MoGood@users.noreply.github.com>
2019-07-05 18:27:05 -0700

    Unit Test for tv-search component

--- src/app/tv-show-search/tv-show-search.component.spec.ts
+++ src/app/tv-show-search/tv-show-search.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TvShowSearchComponent } from './tv-show-search.component';
+import{ MatFormFieldModule, MatInputModule, MatButtonModule, MatToolbarModule, MatCardModule, MatIconModule} from '@angular/material';
+import{FormsModule, ReactiveFormsModule} from '@angular/forms'
+import{ BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('TvShowSearchComponent', () => {
   let component: TvShowSearchComponent;
@@ -8,6 +11,17 @@ describe('TvShowSearchComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        BrowserAnimationsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatCardModule,
+        FormsModule,
+        ReactiveFormsModule,
+      ],
       declarations: [ TvShowSearchComponent ]
     })
     .compileComponents();
